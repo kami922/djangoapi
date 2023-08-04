b21e45d79c07dffa5f6a173ea3700ab672ac75a0
kami992 <kamranulhaq2002@gmail.com>
2023-08-04 21:36:42 +0500

    added api app folder and readme.md

--- companyapi/views.py
+++ companyapi/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse,JsonResponse
 
 def hello(request):
     # return HttpResponse("Hello again <3")
-    a = ["kamran","daim","qabeel"]
+    a = ["kamran","daim","qabeel","moin"]
     return JsonResponse(a,safe=False)
 
     
