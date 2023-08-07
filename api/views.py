0b222f7daa3e9e33a3b49190b12ce9ff417e262a
kami992 <kamranulhaq2002@gmail.com>
2023-08-07 23:22:46 +0500

    third commit added company models and viewset

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+'views'
+from rest_framework import viewsets
+from api.models import Company
+from api.serializers import CompanySerializer
+
 
 # Create your views here.
+'''
+https://www.youtube.com/watch?v=DNFTUtZf1Zc&t=2236s
+46:00'''
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    'class based api view'
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    
--- companyapi/urls.py
+++ companyapi/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",views.hello,name="hello")
+    path("",views.hello,name="hello"),
+    path("api/v1/",include("api.urls")),
     
 ]
